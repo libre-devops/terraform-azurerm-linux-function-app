d571cc7e3366aea70d679031c6fe5ded926ef00c
Craig Thacker <craigthackerx@gmail.com>
2022-04-23 15:53:22 +0100

    add function app

---
+++ fnc-app.tf
@@ -0,0 +1,76 @@
+resource "azurerm_function_app" "function_app" {
+  name = var.function_app_name
+
+  app_service_plan_id        = var.app_service_plan_id
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  storage_account_name       = var.storage_account_name
+  storage_account_access_key = var.storage_account_access_key == null ? azurerm_storage_account.storage[0].primary_access_key : var.storage_account_access_key
+  os_type                    = var.os_type
+
+  app_settings = var.function_app_application_settings
+
+  dynamic "site_config" {
+    for_each = [var.site_config]
+    content {
+      always_on                   = lookup(site_config.value, "always_on", null)
+      ftps_state                  = lookup(site_config.value, "ftps_state", null)
+      http2_enabled               = lookup(site_config.value, "http2_enabled", null)
+      ip_restriction              = lookup(site_config.value, "ip_restriction", null)
+      linux_fx_version            = lookup(site_config.value, "linux_fx_version", null)
+      min_tls_version             = lookup(site_config.value, "min_tls_version", null)
+      pre_warmed_instance_count   = lookup(site_config.value, "pre_warmed_instance_count", null)
+      scm_use_main_ip_restriction = var.scm_authorized_ips != [] || var.scm_authorized_subnet_ids != null || lookup(site_config.value, "scm_ip_restriction", null) != null ? false : true
+      scm_ip_restriction          = concat(var.scm_subnets, var.scm_cidrs, var.scm_service_tags, lookup(site_config.value, "scm_ip_restriction", []))
+      scm_type                    = lookup(site_config.value, "scm_type", null)
+      use_32_bit_worker_process   = lookup(site_config.value, "use_32_bit_worker_process", null)
+      websockets_enabled          = lookup(site_config.value, "websockets_enabled", null)
+
+      dynamic "cors" {
+        for_each = lookup(site_config.value, "cors", []) != [] ? ["fake"] : []
+        content {
+          allowed_origins     = lookup(site_config.value.cors, "allowed_origins", [])
+          support_credentials = lookup(site_config.value.cors, "support_credentials", false)
+        }
+      }
+    }
+  }
+
+  https_only = var.https_only
+
+  lifecycle {
+    ignore_changes = [
+      app_settings.WEBSITE_RUN_FROM_ZIP,
+      app_settings.WEBSITE_RUN_FROM_PACKAGE,
+      app_settings.MACHINEKEY_DecryptionKey,
+      app_settings.WEBSITE_CONTENTAZUREFILECONNECTIONSTRING,
+      app_settings.WEBSITE_CONTENTSHARE
+    ]
+  }
+
+  dynamic "identity" {
+    for_each = length(var.identity_ids) == 0 && var.identity_type == "SystemAssigned" ? [var.identity_type] : []
+    content {
+      type = var.identity_type
+    }
+  }
+
+  dynamic "identity" {
+    for_each = length(var.identity_ids) > 0 || var.identity_type == "UserAssigned" ? [var.identity_type] : []
+    content {
+      type         = var.identity_type
+      identity_ids = length(var.identity_ids) > 0 ? var.identity_ids : []
+    }
+  }
+
+  version = "~${var.function_app_version}"
+
+  tags = var.tags
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "function_vnet_integration" {
+  count = var.function_app_vnet_integration_enabled ? 1 : 0
+
+  app_service_id = azurerm_function_app.function_app.id
+  subnet_id      = var.function_app_vnet_integration_subnet_id
+}
---
+++ input.tf
@@ -0,0 +1,189 @@
+variable "environment" {
+  description = "Project environment"
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name"
+  type        = string
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module."
+  default = {
+    source = "terraform"
+  }
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location."
+  type        = string
+}
+
+variable "location_short" {
+  description = "Short string for Azure location."
+  type        = string
+}
+
+variable "function_app_version" {
+  description = "Version of the function app runtime to use (Allowed values 2 or 3)"
+  type        = number
+  default     = 3
+}
+
+variable "storage_account_access_key" {
+  description = "Access key the storage account to use. If null a new storage account is created"
+  type        = string
+  default     = null
+}
+
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type        = string
+  default     = "StorageV2"
+}
+
+variable "storage_account_min_tls_version" {
+  description = "Storage Account minimal TLS version"
+  type        = string
+  default     = "TLS1_2"
+}
+
+variable "storage_account_enable_advanced_threat_protection" {
+  description = "Boolean flag which controls if advanced threat protection is enabled, see [here](https://docs.microsoft.com/en-us/azure/storage/common/storage-advanced-threat-protection?tabs=azure-portal) for more information."
+  type        = bool
+  default     = false
+}
+
+variable "storage_account_enable_https_traffic_only" {
+  description = "Boolean flag which controls if https traffic only is enabled."
+  type        = bool
+  default     = true
+}
+
+variable "app_service_plan_id" {
+  description = "Id of the App Service Plan for Function App hosting"
+  type        = string
+}
+
+variable "function_language_for_linux" {
+  description = "Language of the Function App on Linux hosting, can be \"dotnet\", \"node\" or \"python\""
+  type        = string
+  default     = "dotnet"
+}
+
+variable "application_insights_enabled" {
+  description = "Enable or disable the Application Insights deployment"
+  type        = bool
+  default     = true
+}
+
+variable "application_insights_id" {
+  description = "ID of the existing Application Insights to use instead of deploying a new one."
+  type        = string
+  default     = null
+}
+
+variable "application_insights_type" {
+  description = "Application Insights type if need to be generated"
+  type        = string
+  default     = "web"
+}
+
+variable "function_app_application_settings" {
+  description = "Function App application settings"
+  type        = map(string)
+  default     = {}
+}
+
+variable "identity_type" {
+  description = "Add an Identity (MSI) to the function app. Possible values are SystemAssigned or UserAssigned"
+  type        = string
+  default     = "SystemAssigned"
+}
+
+variable "identity_ids" {
+  description = "UserAssigned Identities ID to add to Function App. Mandatory if type is UserAssigned"
+  type        = list(string)
+  default     = null
+}
+
+variable "os_type" {
+  description = "A string indicating the Operating System type for this function app."
+  type        = string
+  default     = null
+}
+
+variable "authorized_ips" {
+  description = "IPs restriction for Function. See documentation https://www.terraform.io/docs/providers/azurerm/r/function_app.html#ip_restriction"
+  type        = list(string)
+  default     = []
+}
+
+variable "authorized_subnet_ids" {
+  description = "Subnets restriction for Function. See documentation https://www.terraform.io/docs/providers/azurerm/r/function_app.html#ip_restriction"
+  type        = list(string)
+  default     = []
+}
+
+variable "ip_restriction_headers" {
+  description = "IPs restriction headers for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#headers"
+  type        = map(list(string))
+  default     = null
+}
+
+variable "function_app_vnet_integration_enabled" {
+  description = "Enable VNET integration with the Function App. `function_app_vnet_integration_subnet_id` is mandatory if enabled"
+  type        = bool
+  default     = false
+}
+
+variable "function_app_vnet_integration_subnet_id" {
+  description = "ID of the subnet to associate with the Function App (VNet integration)"
+  type        = string
+  default     = null
+}
+
+# SCM parameters
+
+variable "scm_authorized_ips" {
+  description = "SCM IPs restriction for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#scm_ip_restriction"
+  type        = list(string)
+  default     = []
+}
+
+variable "scm_authorized_subnet_ids" {
+  description = "SCM subnets restriction for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#scm_ip_restriction"
+  type        = list(string)
+  default     = []
+}
+
+variable "scm_ip_restriction_headers" {
+  description = "IPs restriction headers for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#scm_ip_restriction"
+  type        = map(list(string))
+  default     = null
+}
+
+variable "site_config" {
+  description = "Site config for App Service. See documentation https://www.terraform.io/docs/providers/azurerm/r/app_service.html#site_config. IP restriction attribute is not managed in this block."
+  type        = any
+  default     = {}
+}
+
+variable "https_only" {
+  description = "Disable http procotol and keep only https"
+  type        = bool
+  default     = true
+}
+
+variable "application_zip_package_path" {
+  description = "Local or remote path of a zip package to deploy on the Function App"
+  type        = string
+  default     = null
+}
